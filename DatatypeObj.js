cd7f46ee0445fe4dd91f83b70896d80942912b29
tranductri138 <tritocao@gmail.com>
2021-08-31 05:00:02 +0700

    coomit 4h sang

--- DatatypeObj.js
+++ DatatypeObj.js
@@ -5,3 +5,17 @@ let person = {
   address: "Ha Noi",
 };
 console.log(typeof person);
+
+let aa = () => {
+  console.log(
+    "🚀 ~ file: DatatypeObj.js ~ line 12 ~ aa ~ as tris dep zai hihi"
+  );
+};
+console.log("🚀 ~ file: DatatypeObj.js ~ line 15 ~ aa ~ aa", typeof aa);
+aa();
+
+let tris = {
+  name: "tris",
+  age: 23,
+  email: "tritocao@gmail.com",
+};
